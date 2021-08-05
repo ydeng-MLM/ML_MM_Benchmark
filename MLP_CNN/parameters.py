0f5b36c4486abb4a3042f84f9e61f74969cbe4c8
Yang Deng <yd105@research-tarokhlab-08.oit.duke.edu>
2021-08-05 15:06:06 -0400

    08/05/21 best ADM hyperparameters updates

--- MLP_CNN/parameters.py
+++ MLP_CNN/parameters.py
@@ -1,64 +1,30 @@
 """
 Parameter file for specifying the running parameters for forward model
 """
-#DATA_SET = 'ADM'
+DATA_SET = 'ADM'
 #DATA_SET = 'Peurifoy'
-DATA_SET = 'color'
-
-'''
-# Linear layer set up if use FC+TCONV hybrid network, this architecture was tuned for 60k ADM dataset
-USE_CONV = True
-LINEAR = [14, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 250]
-CONV_OUT_CHANNEL = [4, 4, 4, 4]
-CONV_KERNEL_SIZE = [8, 8, 5, 5]
-CONV_STRIDE = [2, 2, 1, 1]
-'''
-
-'''
-#Set up for using FC layers only
-USE_CONV = False
-LINEAR = [14, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000]
-'''
-
-
-'''
-#Current set up for using transpose convolutional layers only
-USE_CONV = True
-LINEAR = [14, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 32000]      #Use FC network to enlarge the sequence first
-CONV_OUT_CHANNEL = [4, 4, 4, 4, 4, 4]
-CONV_KERNEL_SIZE = [8, 8, 8, 8, 5, 5]
-CONV_STRIDE = [2, 2, 2, 2, 1, 1]
-'''
-
-'''
-#Set up for using channels, this architecture was tuned for 60k ADM dataset
-USE_CONV = True
-LINEAR = [14, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000]      #Still using two FC layers to have compatiable number of input parameters to tconv layers
-CONV_OUT_CHANNEL = [4, 64, 128, 256, 512]
-CONV_KERNEL_SIZE = [5, 8, 8, 8, 5]
-CONV_STRIDE = [1, 2, 2, 2, 1]
-'''
+#DATA_SET = 'color'
 
 SKIP_CONNECTION = True
 USE_CONV = False
-#LINEAR = [14, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2001]
-LINEAR = [3, 500, 500, 500, 500, 500, 500, 500, 500, 3]      #Still using two FC layers to have compatiable number of input parameters to tconv layers
+LINEAR = [14, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2001]
+#LINEAR = [3, 500, 500, 500, 500, 500, 500, 500, 500, 3]      #Still using two FC layers to have compatiable number of input parameters to tconv layers
 CONV_OUT_CHANNEL = [4, 4, 4, 8, 8]
 CONV_KERNEL_SIZE = [3, 24, 24, 24, 3]
 CONV_STRIDE = [1, 2, 2, 2, 1]
 
 # Hyperparameters
 OPTIM = "Adam"
-REG_SCALE = 5e-4
+REG_SCALE = 1e-4
 BATCH_SIZE = 1024
 EVAL_STEP = 20
 TRAIN_STEP = 500
 LEARN_RATE = 1e-4
 LR_DECAY_RATE = 0.2
 STOP_THRESHOLD = 1e-9
-DROPOUT = 0
-SKIP_HEAD = 0
-SKIP_TAIL = [2, 4, 6]
+DROPOUT = 0.1
+SKIP_HEAD = 7
+SKIP_TAIL = [2, 4, 6] #Currently useless
 
 # Data Specific params
 X_RANGE = [i for i in range(2, 16)]     #60k dataset sepcific, first two columns are index and 2-16 are geoemtry parameters
