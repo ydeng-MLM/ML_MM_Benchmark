385e594e4db40214054ee1202e3ca7900140c315
Simiao Ren <sr365@research-tarokhlab-04.oit.duke.edu>
2021-08-26 21:51:54 -0400

    updated utils functions and the transformer useless functions

--- Transformer/class_wrapper.py
+++ Transformer/class_wrapper.py
@@ -48,9 +48,6 @@ class Network(object):
         Function to create the network module from provided model fn and flags
         :return: the created nn module
         """
-        # encoder = Encoder(self.flags)
-        # decoder = Decoder(self.flags)
-        # spec_enc = SpectraEncoder(self.flags)
         model = self.model_fn(self.flags)
         print(model)
         return model
@@ -92,27 +89,6 @@ class Network(object):
             raise Exception("Your Optimizer is neither Adam, RMSprop or SGD, please change in param or contact Ben")
         return op
     
-    def make_optimizer_eval(self, geometry_eval, optimizer_type=None):
-        """
-        The function to make the optimizer during evaluation time.
-        The difference between optm is that it does not have regularization and it only optmize the self.geometr_eval tensor
-        :return: the optimizer_eval
-        """
-        if optimizer_type is None:
-            optimizer_type = self.flags.optim
-        if optimizer_type == 'Adam':
-            op = torch.optim.Adam([geometry_eval], lr=self.flags.lr)
-        elif optimizer_type == 'RMSprop':
-            op = torch.optim.RMSprop([geometry_eval], lr=self.flags.lr)
-        elif optimizer_type == 'SGD':
-            op = torch.optim.SGD([geometry_eval], lr=self.flags.lr)
-        else:
-            raise Exception("Your Optimizer is neither Adam, RMSprop or SGD, please change in param or contact Ben")
-        return op
-
-    def build_tensor(self, nparray, requires_grad=False):
-        return torch.tensor(nparray, requires_grad=requires_grad, device='cuda', dtype=torch.float)
-
     def make_lr_scheduler(self, optm):
         """
         Make the learning rate scheduler as instructed. More modes can be added to this, current supported ones:
@@ -143,6 +119,9 @@ class Network(object):
         self.model = torch.load(os.path.join(self.ckpt_dir, 'best_model_forward.pt'))
     
     def get_cuda_memory_info(self):
+        """
+        The function that gets the cuda memory to check for a memory problem
+        """
         r = torch.cuda.memory_reserved(0)/1e9
         a = torch.cuda.memory_allocated(0)/1e9
         print('after delete, reserved memory {}G, allocated memory {}G'.format(r,a))
@@ -179,10 +158,6 @@ class Network(object):
                 self.optm.step()                                    # Move one step the optimizer
                 train_loss += loss.cpu().data.numpy()                                  # Aggregate the loss
                 del S_pred, loss
-                ################ Debugging the memory cuda #################
-                #print('in epoch {} test batch {}, lenghth is :'.format(epoch, j))
-                #self.get_cuda_memory_info()
-                ################ Debugging the memory cuda #################
 
             # Calculate the avg loss of training
             train_avg_loss = train_loss / (j + 1)
@@ -202,20 +177,12 @@ class Network(object):
                     if cuda:
                         geometry = geometry.cuda()
                         spectra = spectra.cuda()
-                    ################ Debugging the memory cuda #################
-                    #print('in epoch {} test batch {}, lenghth is :'.format(epoch, j))
-                    #self.get_cuda_memory_info()
-                    ################ Debugging the memory cuda #################
 
 
                     S_pred = self.model(geometry)
                     loss = self.make_loss(logit=S_pred, labels=spectra)
-                    #print(loss.cpu().data.numpy())
                     test_loss += loss.cpu().data.numpy()                                       # Aggregate the loss
                     del loss, S_pred
-                    ################ Debugging the memory cuda #################
-                    #self.get_cuda_memory_info()
-                    ################ Debugging the memory cuda #################
 
                 # Record the testing loss to the tensorboard
                 test_avg_loss = test_loss/ (j+1)
@@ -268,206 +235,4 @@ class Network(object):
                 np.savetxt(fyt, spectra.cpu().data.numpy())
                 np.savetxt(fyp, Ypred)
         tk.record(1)                # Record the total time of the eval period
-        return Ypred_file, Ytruth_file
-    
-
-    def evaluate_multiple_time(self, time=200, save_dir='/home/sr365/mm_bench_multi_eval/VAE/'):
-        """
-        Make evaluation multiple time for deeper comparison for stochastic algorithms
-        :param save_dir: The directory to save the result
-        :return:
-        """
-        save_dir = os.path.join(save_dir, self.flags.data_set)
-        tk = time_keeper(os.path.join(save_dir, 'evaluation_time.txt'))
-        if not os.path.isdir(save_dir):
-            os.makedirs(save_dir)
-        for i in range(time):
-            self.evaluate(save_dir=save_dir, prefix='inference' + str(i))
-            tk.record(i)
-
-
-    def evaluate_inverse(self, save_dir='data/', save_all=False, MSE_Simulator=False, save_misc=False, save_Simulator_Ypred=True):
-        """
-        The function to evaluate how good the Neural Adjoint is and output results
-        :param save_dir: The directory to save the results
-        :param save_all: Save all the results instead of the best one (T_200 is the top 200 ones)
-        :param MSE_Simulator: Use simulator loss to sort (DO NOT ENABLE THIS, THIS IS OK ONLY IF YOUR APPLICATION IS FAST VERIFYING)
-        :param save_misc: save all the details that are probably useless
-        :param save_Simulator_Ypred: Save the Ypred that the Simulator gives
-        (This is useful as it gives us the true Ypred instead of the Ypred that the network "thinks" it gets, which is
-        usually inaccurate due to forward model error)
-        :return:
-        """
-        self.load()                             # load the model as constructed
-        try:
-            bs = self.flags.backprop_step         # for previous code that did not incorporate this
-        except AttributeError:
-            print("There is no attribute backprop_step, catched error and adding this now")
-            self.flags.backprop_step = 300
-        cuda = True if torch.cuda.is_available() else False
-        if cuda:
-            self.model.cuda()
-        self.model.eval()
-        saved_model_str = self.saved_model.replace('/','_')
-        # Get the file names
-        Ypred_file = os.path.join(save_dir, 'test_Ypred_{}.csv'.format(saved_model_str))
-        Xtruth_file = os.path.join(save_dir, 'test_Xtruth_{}.csv'.format(saved_model_str))
-        Ytruth_file = os.path.join(save_dir, 'test_Ytruth_{}.csv'.format(saved_model_str))
-        Xpred_file = os.path.join(save_dir, 'test_Xpred_{}.csv'.format(saved_model_str))
-        print("evalution output pattern:", Ypred_file)
-
-        # Time keeping
-        #tk = time_keeper(time_keeping_file=os.path.join(save_dir, 'evaluation_time.txt'))
-
-        # Open those files to append
-        with open(Xtruth_file, 'a') as fxt,open(Ytruth_file, 'a') as fyt,\
-                open(Ypred_file, 'a') as fyp, open(Xpred_file, 'a') as fxp:
-            # Loop through the eval data and evaluate
-            for ind, (geometry, spectra) in enumerate(self.test_loader):
-                if cuda:
-                    geometry = geometry.cuda()
-                    spectra = spectra.cuda()
-                # Initialize the geometry first
-                Xpred, Ypred, loss = self.evaluate_one(spectra, save_dir=save_dir, save_all=save_all, ind=ind,
-                                                        MSE_Simulator=MSE_Simulator, save_misc=save_misc, save_Simulator_Ypred=save_Simulator_Ypred)
-                #tk.record(ind)                          # Keep the time after each evaluation for backprop
-                # self.plot_histogram(loss, ind)                                # Debugging purposes
-                np.savetxt(fxt, geometry.cpu().data.numpy())
-                np.savetxt(fyt, spectra.cpu().data.numpy())
-                if self.flags.data_set != 'meta_material':
-                    np.savetxt(fyp, Ypred)
-                np.savetxt(fxp, Xpred)
-                if ind > 10:
-                    break
-        return Ypred_file, Ytruth_file
-    
-    def evaluate_one(self, target_spectra, save_dir='data/', MSE_Simulator=False ,save_all=False, ind=None, save_misc=False, save_Simulator_Ypred=False):
-        """
-        The function which being called during evaluation and evaluates one target y using # different trails
-        :param target_spectra: The target spectra/y to backprop to 
-        :param save_dir: The directory to save to when save_all flag is true
-        :param MSE_Simulator: Use Simulator Loss to get the best instead of the default NN output logit
-        :param save_all: The multi_evaluation where each trail is monitored (instad of the best) during backpropagation
-        :param ind: The index of this target_spectra in the batch
-        :param save_misc: The flag to print misc information for degbugging purposes, usually printed to best_mse
-        :return: Xpred_best: The 1 single best Xpred corresponds to the best Ypred that is being backproped 
-        :return: Ypred_best: The 1 singe best Ypred that is reached by backprop
-        :return: MSE_list: The list of MSE at the last stage
-        """
-
-        # Initialize the geometry_eval or the initial guess xs
-        geometry_eval = self.initialize_geometry_eval()
-        # Set up the learning schedule and optimizer
-        self.optm_eval = self.make_optimizer_eval(geometry_eval)#, optimizer_type='SGD')
-        self.lr_scheduler = self.make_lr_scheduler(self.optm_eval)
-        
-        # expand the target spectra to eval batch size
-        target_spectra_expand = target_spectra.expand([self.flags.eval_batch_size, -1])
-
-        # Begin NA
-        for i in range(self.flags.backprop_step):
-            # Make the initialization from [-1, 1], can only be in loop due to gradient calculator constraint
-            geometry_eval_input = self.initialize_from_uniform_to_dataset_distrib(geometry_eval)
-            self.optm_eval.zero_grad()                                  # Zero the gradient first
-            logit = self.model(geometry_eval_input)                     # Get the output
-            ###################################################
-            # Boundar loss controled here: with Boundary Loss #
-            ###################################################
-            loss = self.make_loss(logit, target_spectra_expand, G=geometry_eval_input)         # Get the loss
-            loss.backward()                                             # Calculate the Gradient
-            # update weights and learning rate scheduler
-            if i != self.flags.backprop_step - 1:
-                self.optm_eval.step()  # Move one step the optimizer
-                self.lr_scheduler.step(loss.data)
-        
-        ###################################
-        # From candidates choose the best #
-        ###################################
-        Ypred = logit.cpu().data.numpy()
-
-        # calculate the MSE list and get the best one
-        MSE_list = np.mean(np.square(Ypred - target_spectra_expand.cpu().data.numpy()), axis=1)
-        best_estimate_index = np.argmin(MSE_list)
-        print("The best performing one is:", best_estimate_index)
-        Xpred_best = np.reshape(np.copy(geometry_eval_input.cpu().data.numpy()[best_estimate_index, :]), [1, -1])
-        Ypred_best = np.reshape(np.copy(Ypred[best_estimate_index, :]), [1, -1])
-
-        return Xpred_best, Ypred_best, MSE_list
-
-    def initialize_geometry_eval(self):
-        """
-        Initialize the geometry eval according to different dataset. These 2 need different handling
-        :return: The initialized geometry eval
-        """
-        geomtry_eval = torch.rand([self.flags.eval_batch_size, self.flags.dim_G], requires_grad=True, device='cuda')
-        #geomtry_eval = torch.randn([self.flags.eval_batch_size, self.flags.linear[0]], requires_grad=True, device='cuda')
-        return geomtry_eval
-
-    def initialize_from_uniform_to_dataset_distrib(self, geometry_eval):
-        """
-        since the initialization of the backprop is uniform from [0,1], this function transforms that distribution
-        to suitable prior distribution for each dataset. The numbers are accquired from statistics of min and max
-        of the X prior given in the training set and data generation process
-        :param geometry_eval: The input uniform distribution from [0,1]
-        :return: The transformed initial guess from prior distribution
-        """
-        X_range, X_lower_bound, X_upper_bound = self.get_boundary_lower_bound_uper_bound()
-        geometry_eval_input = geometry_eval * self.build_tensor(X_range) + self.build_tensor(X_lower_bound)
-        return geometry_eval_input
-    
-    def get_boundary_lower_bound_uper_bound(self):
-        """
-        Due to the fact that the batched dataset is a random subset of the training set, mean and range would fluctuate.
-        Therefore we pre-calculate the mean, lower boundary and upper boundary to avoid that fluctuation. Replace the
-        mean and bound of your dataset here
-        :return:
-        """
-        if self.flags.data_set == 'Peurifoy': 
-            x_dim = 8
-            return np.array([2 for i in range(x_dim)]), np.array([-1 for i in range(x_dim)]), np.array([1 for i in range(x_dim)])
-        elif self.flags.data_set == 'Color': 
-            x_dim = 3
-            return np.array([1 for i in range(x_dim)]), np.array([0 for i in range(x_dim)]), np.array([1 for i in range(x_dim)])
-        elif self.flags.data_set == 'Yang': 
-            x_max = np.array([6.000000238418579102e-01, 1.500000000000000000e+00,2.000000029802322388e-01, 2.000000029802322388e-01
-                    ,2.000000029802322388e-01,2.000000029802322388e-01,2.000000029802322388e-01,2.000000029802322388e-01
-                    ,2.000000029802322388e-01,2.000000029802322388e-01,7.853999733924863502e-01,7.853999733924863502e-01
-                    ,7.853999733924863502e-01,7.853999733924863502e-01])
-            x_min = np.array([3.000000119209288996e-01,1.000000000000000000e+00,1.000000014901161471e-01,1.000000014901161471e-01
-                    ,1.000000014901161471e-01,1.000000014901161471e-01,1.000000014901161471e-01,1.000000014901161471e-01
-                    ,1.000000014901161471e-01,1.000000014901161471e-01,-7.853999733924863502e-01,-7.853999733924863502e-01
-                    ,-7.853999733924863502e-01,-7.853999733924863502e-01])
-            return x_max - x_min, x_min, x_max
-
-    def predict(self, Ytruth_file, save_dir='data/', prefix=''):
-        self.load()                             # load the model as constructed
-        cuda = True if torch.cuda.is_available() else False
-        if cuda:
-            self.model.cuda()
-        self.model.eval()
-        saved_model_str = self.saved_model.replace('/', '_') + prefix
-
-        Ytruth = pd.read_csv(Ytruth_file, header=None, delimiter=',')     # Read the input
-        if len(Ytruth.columns) == 1: # The file is not delimitered by ',' but ' '
-            Ytruth = pd.read_csv(Ytruth_file, header=None, delimiter=' ')
-        Ytruth_tensor = torch.from_numpy(Ytruth.values).to(torch.float)
-        print('shape of Ytruth tensor :', Ytruth_tensor.shape)
-
-        # Get the file names
-        Ypred_file = os.path.join(save_dir, 'test_Ypred_{}.csv'.format(saved_model_str))
-        Ytruth_file = os.path.join(save_dir, 'test_Ytruth_{}.csv'.format(saved_model_str))
-        Xpred_file = os.path.join(save_dir, 'test_Xpred_{}.csv'.format(saved_model_str))
-        # keep time
-        tk = time_keeper(os.path.join(save_dir, 'evaluation_time.txt'))
-    
-        if cuda:
-            Ytruth_tensor = Ytruth_tensor.cuda()
-        print('model in eval:', self.model)
-        Xpred = self.model.inference(Ytruth_tensor).cpu().data.numpy()
-
-        # Open those files to append
-        with open(Ytruth_file, 'a') as fyt, open(Ypred_file, 'a') as fyp, open(Xpred_file, 'a') as fxp:
-            np.savetxt(fyt, Ytruth_tensor.cpu().data.numpy())
-            np.savetxt(fxp, Xpred)
-        tk.record(1)
         return Ypred_file, Ytruth_file
--- Transformer/evaluate.py
+++ Transformer/evaluate.py
@@ -30,31 +30,19 @@ def predict(model_dir, Ytruth_file ,multi_flag=False):
         flags = helper_functions.load_flags(os.path.join("models", model_dir))
     flags.eval_model = model_dir                    # Reset the eval mode
     
+    # Create network from class wrapper 
     ntwk = Network(Transformer, flags, train_loader=None, test_loader=None, 
                     inference_mode=True, saved_model=flags.eval_model)
+    
+    # Get the total number of trainable parameters
     print("number of trainable parameters is :")
     pytorch_total_params = sum(p.numel() for p in ntwk.model.parameters() if p.requires_grad)
     print(pytorch_total_params)
+
     # Evaluation process
     pred_file, truth_file = ntwk.predict(Ytruth_file)
     if 'Yang' not in flags.data_set:
         plotMSELossDistrib(pred_file, truth_file, flags)
-
-
-def predict_different_dataset(multi_flag=False):
-    """
-    This function is to evaluate all different datasets in the model with one function call
-    """
-    step_func_dir = '/home/sr365/MM_Bench/Data/step_func'
-    for model in os.listdir('models/'):
-        if 'best' in model:
-            if 'Yang' in model:
-                Ytruth_file = os.path.join(step_func_dir, 'Yang'+'step_function.txt')
-            elif 'Chen' in model:
-                Ytruth_file = os.path.join(step_func_dir, 'Chen'+'step_function.txt')
-            elif 'Peurifoy' in model:
-                Ytruth_file = os.path.join(step_func_dir, 'Peurifoy'+'step_function.txt')
-            predict(model, Ytruth_file, multi_flag=multi_flag)
 
 
 def evaluate_from_model(model_dir):
@@ -107,56 +95,10 @@ def evaluate_different_dataset(multi_flag=False, eval_data_all=False, modulized_
     This function is to evaluate all different datasets in the model with one function call
     """
     for model in os.listdir('models/'):
+        # Only go through the best models chosen from your validation set error
         if 'best' in model:
             evaluate_from_model(model, multi_flag=multi_flag, 
                         eval_data_all=eval_data_all, modulized_flag=modulized_flag)
-
-def evaluate_inverse_from_model(model_dir,  eval_data_all=False, save_misc=False, MSE_Simulator=False, save_Simulator_Ypred=False):
-
-    """
-    Evaluating interface. 1. Retreive the flags 2. get data 3. initialize network 4. eval
-    :param model_dir: The folder to retrieve the model
-    :param multi_flag: The switch to turn on if you want to generate all different inference trial results
-    :param eval_data_all: The switch to turn on if you want to put all data in evaluation data
-    :return: None
-    """
-    # Retrieve the flag object
-    print("Retrieving flag object for parameters")
-    if (model_dir.startswith("models")):
-        model_dir = model_dir[7:]
-        print("after removing prefix models/, now model_dir is:", model_dir)
-    print(model_dir)
-    flags = load_flags(os.path.join("models", model_dir))
-    flags.eval_model = model_dir                    # Reset the eval mode
-    flags.backprop_step = 300
-    flags.test_ratio = get_test_ratio_helper(flags)
-    flags.batch_size = 1                            # For backprop eval mode, batchsize is always 1
-    flags.lr = 0.5
-    flags.lr_decay_rate = 0.5
-    flags.eval_batch_size = 2048
-    # flags.train_step = eval_flags.train_step
-
-    print(flags)
-
-    # Get the data
-    train_loader, test_loader = data_reader.read_data(flags, eval_data_all=True)
-    print("Making network now")
-    
-    # Make Network
-    ntwk = Network(Transformer, flags, train_loader, test_loader, inference_mode=True, saved_model=flags.eval_model)
-    print("number of trainable parameters is :")
-    pytorch_total_params = sum(p.numel() for p in ntwk.model.parameters() if p.requires_grad)
-    print(pytorch_total_params)
-    
-
-    # Evaluation process
-    print("Start eval now:")
-    pred_file, truth_file = ntwk.evaluate_inverse(save_misc=save_misc, MSE_Simulator=MSE_Simulator, save_Simulator_Ypred=save_Simulator_Ypred)
-
-    # Plot the MSE distribution
-    plotMSELossDistrib(pred_file, truth_file, flags)
-    print("Evaluation finished")
-
 
 if __name__ == '__main__':
     # Read the flag, however only the flags.eval_model is used and others are not used
@@ -164,28 +106,4 @@ if __name__ == '__main__':
 
     print(useless_flags.eval_model)
     evaluate_inverse_from_model('best_models/Color')
-    ##########################
-    #Single model evaluation #
-    ##########################
-    ### Call the evaluate function from model, this "evaluate_from_model" uses the eval_model field in your
-    ### "useless_flag" that reads out from your current parameters.py file in case you want to evaluate single model
-    #evaluate_from_model(useless_flags.eval_model)
-    #evaluate_from_model(useless_flags.eval_model, multi_flag=True)
-    #evaluate_from_model(useless_flags.eval_model, multi_flag=False, eval_data_all=True)
     
-    #evaluate_from_model("models/Peurifoy_feature_channel_8_natthead_8_dim_fc_128")
-    
-    ############################
-    #Multiple model evaluation #
-    ############################
-    ### Call the "evaluate_different_dataset" function to evaluate all the models in the "models" folder, the multi_flag is to control whether evaulate across T or only do T=1 (if set to False), make sure you change the model name in function if you have any different model name 
-    #evaluate_different_dataset(multi_flag=False, eval_data_all=False)
-    #evaluate_different_dataset(multi_flag=True, eval_data_all=False)
-    #evaluate_different_dataset(multi_flag=True)
-    #evaluate_all("models/best_models/")
-    #evaluate_all("models/Peurifoy_4th/")
-
-    ###########
-    # Predict #
-    ###########
-    #predict_different_dataset(multi_flag=False)
